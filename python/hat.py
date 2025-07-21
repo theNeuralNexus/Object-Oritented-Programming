5d48a1e3be66b2aced1bdb532c7499ea8b9384f5
abs <abdulsalam.network@gmail.com>
2025-07-21 14:35:42 +0500

    Update

---
+++ python/hat.py
@@ -0,0 +1,13 @@
+# Learning about class methods and class variables
+import random
+
+class Hat:
+    houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+
+
+    @classmethod
+    def sort(cls, name):
+        print(f"{name} is in {random.choice(cls.houses)}")
+
+
+Hat.sort("Harry")
